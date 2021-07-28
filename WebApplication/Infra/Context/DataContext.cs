8f73db9681e4e27bcc34a64453f4ae8d9e2f3b1a
caio ribeiro <caio.fstelcom@gmail.com>
2021-07-28 18:01:30 -0300

    crud

---
+++ WebApplication/Infra/Context/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models.Entities;
+
+namespace WebApplication.Infra.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opt):base(opt)
+        {
+
+        }
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
